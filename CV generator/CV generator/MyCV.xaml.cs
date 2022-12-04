9d71bd904b1bd16265c3bd89176ad3c52b80f5c2
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2022-12-04 07:20:54 -0500

    Agregar archivos de proyecto.

---
+++ CV generator/CV generator/MyCV.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CV_generator
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MyCV : ContentPage
+	{
+		public MyCV ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
